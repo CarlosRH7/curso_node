65b6967ae289fbcc02ccaf3cf610ca012f5214d0
Carlos Alberto <c.ramirez@aqtiva.mx>
2017-07-05 18:49:43 -0500

    primer version

---
+++ servidor.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+var contenido = fs.readFileSync('/Users/carlos/Desktop/curso_node/destino.html');
+
+console.log(contenido);
+console.log('haga otro clic 1 ');
+console.log('haga otro clic 2');
+console.log('haga otro clic 3');
+console.log('haga otro clic 4');
